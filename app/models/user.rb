1f11d9007305f0a24252f7e3a4283c3072a73b4b
wadror <wadror@wadror-VirtualBox.(none)>
2015-02-08 17:22:24 +0200

    ex 3-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,21 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 4 }
 
   validates :password, format: { with: /\d.*[A-Z]|[A-Z].*\d/,  message: "has to contain one number and one upper case letter" }
+
+  def favorite_beer
+    return nil if ratings.empty?   # palautetaan nil jos reittauksia ei ole
+    ratings.order(score: :desc).limit(1).first.beer
+  end
+
+  def favorite_style
+    return nil if ratings.empty?
+    ratings.group_by { |r| r.beer.style }.map { |s, rs| [s, rs.map{|r| r.score}.sum / rs.size.to_f] }
+    .sort_by { |x, y| y}.last.first
+  end
+
+  def favorite_brewery
+    return nil if beers.empty?
+    User.first.ratings.group_by {|r| r.beer.brewery }.map { |s, rs| [s, rs.map{|r| r.score}.sum / rs.size.to_f] }
+    .last.first.name
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,7 +29,7 @@ describe User do
   end
 
   describe "with a proper password" do
-    let(:user){ User.create username:"Pekka", password:"Secret1", password_confirmation:"Secret1" }
+    let(:user){ FactoryGirl.create(:user) }
 
     it "is saved" do
       expect(user).to be_valid
@@ -37,14 +37,71 @@ describe User do
     end
 
     it "and with two ratings, has the correct average rating" do
-      rating = Rating.new score:10
-      rating2 = Rating.new score:20
-
-      user.ratings << rating
-      user.ratings << rating2
+      user.ratings << FactoryGirl.create(:rating)
+      user.ratings << FactoryGirl.create(:rating2)
 
       expect(user.ratings.count).to eq(2)
       expect(user.average_rating).to eq(15.0)
+    end
+  end
+
+  describe "favorite beer" do
+    let(:user){FactoryGirl.create(:user) }
+
+    it "has method for determining one" do
+      user.should respond_to :favorite_beer
+    end
+
+    it "without ratings does not have one" do
+      expect(user.favorite_beer).to eq(nil)
+    end
+
+    it "is the only rated if only one rating" do
+      beer = FactoryGirl.create(:beer)
+      rating = FactoryGirl.create(:rating, beer:beer, user:user)
+
+      expect(user.favorite_beer).to eq(beer)
+    end
+
+    it "is the one with highest rating if several rated" do
+      create_beers_with_ratings(10, 20, 15, 7, 9, user)
+      best = create_beer_with_rating(25, user)
+
+      expect(user.favorite_beer).to eq(best)
+    end
+  end
+
+  describe "favorite style" do
+    let(:user){FactoryGirl.create(:user) }
+    it "one with highest average rating" do
+      beer1 = FactoryGirl.create(:beer)
+      FactoryGirl.create(:rating, score:10, beer:beer1, user:user)
+      beer2 = FactoryGirl.create(:beer2)
+      FactoryGirl.create(:rating, score:15, beer:beer2, user:user)
+      beer3 = FactoryGirl.create(:beer3)
+      FactoryGirl.create(:rating, score:31, beer:beer3, user:user)
+      expect(user.favorite_style).to eq(beer3.style)
+    end
+  end
+
+  describe "favorite brewery" do
+    let(:user){FactoryGirl.create(:user) }
+    it "one with highest rating average" do
+      beer1 = FactoryGirl.create(:beer)
+      FactoryGirl.create(:rating, score:10, beer:beer1, user:user)
+      expect(user.favorite_brewery).to eq (beer1.brewery.name)
+    end
+  end
+
+  def create_beer_with_rating(score, user)
+    beer = FactoryGirl.create(:beer)
+    FactoryGirl.create(:rating, score:score, beer:beer, user:user)
+    beer
+  end
+
+  def create_beers_with_ratings(*scores, user)
+    scores.each do |score|
+      create_beer_with_rating score, user
     end
   end
 end
